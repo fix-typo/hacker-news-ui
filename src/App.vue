f49c483558e048f08bfc4f58fe6c7179464470d1
qzda <qinzhidaoa@gmail.com>
2024-08-03 16:07:24 +0800

    feat: import element-plus

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
   import { onMounted, ref, watch } from "vue";
+  import { ElCard, ElPagination } from "element-plus";
+  import { Loading } from "@element-plus/icons-vue";
   import { fetchItemById, fetchTopStories } from "./server";
   import dayjs from "dayjs";
   import { Item } from "./types";
@@ -36,36 +38,53 @@
 </script>
 
 <template>
-  <div class="p-2">
-    <h2>topstories {{ topstoriesID.length }}</h2>
+  <div class="p-4">
+    <h2 class="m0 mb-4">Topstories {{ topstoriesID.length }}</h2>
     <div
-      v-for="(storie, index) in topstories"
-      class="box mb-2 hover:bg-gray-200">
-      <div>
-        {{ index + 1 + (pageNum - 1) * pageSize }}.
-        <a
-          class="font-bold decoration-none c-black"
-          target="_blank"
-          :href="storie.url">
-          {{ storie.title }}
-        </a>
-      </div>
-      <div class="pl-5">
-        {{ storie.score }} points | by {{ storie.by }} |
-        {{ dayjs.unix(storie.time).format("YYYY-MM-DD HH:mm:ss") }} |
-        {{ storie.descendants }} commants
-      </div>
+      v-if="!topstories.length"
+      class="xy-center">
+      <Loading class="w-50 py-10 c-gray-200 animate-rotating" />
     </div>
-    <button @click="pageNum = Math.max(1, pageNum - 1)">prev</button>
-    pageSize:
-    <input
-      class="w-10"
-      type="number"
-      v-model="pageSize" />
-    pageNum: {{ pageNum }}
-    <button
-      @click="pageNum = Math.min(topstoriesID.length / pageSize, pageNum + 1)">
-      next
-    </button>
+    <ElCard
+      v-for="(storie, index) in topstories"
+      class="mb-2"
+      shadow="hover">
+      <div class="flex">
+        <div class="mr-2">{{ index + 1 + (pageNum - 1) * pageSize }}.</div>
+        <div>
+          <div>
+            <a
+              class="font-bold decoration-none c-black"
+              target="_blank"
+              :href="storie.url">
+              {{ storie.title }}
+            </a>
+          </div>
+          {{ storie.score }} points | by {{ storie.by }} |
+          {{ dayjs.unix(storie.time).format("YYYY-MM-DD HH:mm:ss") }} |
+          {{ storie.descendants }} commants
+        </div>
+      </div>
+    </ElCard>
+    <ElPagination
+      v-model:current-page="pageNum"
+      v-model:page-size="pageSize"
+      layout="prev, pager, next, sizes"
+      :total="topstoriesID.length"
+      class="xy-center justify-end" />
   </div>
 </template>
+
+<style scoped>
+  .animate-rotating {
+    animation: rotating 2s linear infinite;
+  }
+  @keyframes rotating {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { createApp } from "vue";
 import "virtual:uno.css";
 import axios from "axios";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
 
 import "./style.css";
 import App from "./App.vue";
 
 axios.defaults.baseURL = "https://hacker-news.firebaseio.com/v0/";
-createApp(App).mount("#app");
+
+createApp(App).use(ElementPlus).mount("#app");
